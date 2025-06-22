6ad4cb58b6055385ea454e34a5c7e3acc1502d2a
Mustafa Qasimzade <lacromis@gmail.com>
2025-06-23 02:42:09 +0400

    design done

---
+++ KlavyeTest/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using KlavyeTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KlavyeTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var words = new List<string>
+        {
+            "elma", "araba", "masa", "kalem", "defter", "bilgisayar", "yazılım", "klavye"
+        };
+
+            int durationInSeconds = 45; // Məsələn, 45 saniyəlik test
+
+            var model = new TypingTestViewModel
+            {
+                Words = words,
+                DurationInSeconds = durationInSeconds
+            };
+
+            return View(model);
+        }
+        public class TypingTestViewModel
+        {
+            public List<string> Words { get; set; }
+            public int DurationInSeconds { get; set; }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KlavyeTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KlavyeTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
